87434668924ccd07f7572a13abc9093d98817bab
Mauricio Almeida <mauriciomdea@gmail.com>
2019-07-02 10:31:23 -0300

    Finishing basic PWA in order to show to client

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
+  helper_method :participante
   # http_basic_authenticate_with name: "admin", password: "cocacola"
+
+  def participante
+    session[:participante_id] = Participante.find_by(login: 'betal').id
+    # if session[:user_id]
+    #   @current_user ||= User.find(session[:user_id])
+    # else
+    #   @current_user = nil
+    # end
+  end
+
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,15 @@
 class WelcomeController < ApplicationController
 
   def index
-    @participante = Participante.find_by(login: request.query_parameters[:participante])
-    @primeiro_dia = @participante.primeiro_dia
-    @segundo_dia = @participante.segundo_dia
+    # @participante = Participante.find_by(login: request.query_parameters[:participante])
+    # @participante = Participante.find_by(login: 'betal')
+    @participante = Participante.find(participante)
+    # @primeiro_dia = @participante.primeiro_dia
+    # @segundo_dia = @participante.segundo_dia
   end
 
   def sobre
+    @participante = participante
   end
 
 end
